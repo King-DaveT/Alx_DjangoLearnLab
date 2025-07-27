57cc0c7dcb67ca7a7f3b5dd6bee904a5b0fbb517
King-DaveT <davidak.tech@gmail.com>
2025-07-27 11:46:18 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,22 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .forms import ExampleForm
+
+from django.shortcuts import render
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process form data (for demonstration, we can just print it)
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            print(name, email)
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
 
 @permission_required('relationship_app.can_view', raise_exception=True)
 def view_books(request):
